9dcb18ae4c2e83160772fff18906e1123722842e
matheus-battiston <matheus_battiston@hotmail.com>
2022-04-21 11:58:07 -0300

    Tudo funcional ??? (65% confiança)

--- GBN.py
+++ GBN.py
@@ -15,19 +15,15 @@ class Sender:
         self.Sn = 0
 
     def incrementa_timer(self):
-        if self.timeout():
-            self.prox_envio = self.primeiro_janela
-            self.timer = 0
-            self.pendentes = 0
-            self.ultimo_janela = self.primeiro_janela
         self.timer += 1
 
     def timeout(self):
-        if self.timer == 100:
-            print('Note over A: TIMEOUT(', self.primeiro_janela+1, ')',sep="")
-            return True
+        print('Note over A: TIMEOUT(', self.primeiro_janela+1, ')',sep="")
+        self.prox_envio = self.primeiro_janela
+        self.timer = 0
+        self.pendentes = 0
+        self.ultimo_janela = self.primeiro_janela
 
-        return False
 
     def pode_enviar(self):
         if self.pendentes < self.janela:
@@ -75,7 +71,9 @@ class Receiver:
         self.seq = self.seq*100
 
     def receive(self, frame):
-        if frame == self.seq[self.Rn]:
+        if self.check_duplicate(frame):
+            self.recebidos.append(frame)
+        elif frame == self.seq[self.Rn]:
             self.recebidos.append(frame)
             self.janela += 1
             self.Rn += 1
@@ -89,6 +87,13 @@ class Receiver:
 
         return False
 
+    def check_duplicate(self, frame):
+        aux = self.Rn -1
+        while aux >= 0 and aux > self.Rn - self.seq_bits:
+            if frame == self.seq[aux]:
+                return True
+            aux -= 1
+        return False
 
 def executar_gbn(num_frames, seq_bits, pkt_loss):
     sucesso = 0
@@ -98,16 +103,15 @@ def executar_gbn(num_frames, seq_bits, pkt_loss):
     enviados = []
 
     while sucesso < num_frames:
-        sender.incrementa_timer()
         while sender.pode_enviar() and sender.ultimo_janela < num_frames:
             enviado, frame = sender.send()
             if pacotes not in pkt_loss:
+                receiver.receive(enviado)
                 if frame not in enviados:
                     print("A ->> B : (", frame, ") Frame ", enviado, sep="")
                 else:
                     print("A ->> B : (", frame, ") Frame ", enviado, " (RET)",sep="")
 
-                receiver.receive(enviado)
             else:
                 print("A -x B : (", frame, ") Frame ", enviado, sep="")
 
@@ -124,3 +128,6 @@ def executar_gbn(num_frames, seq_bits, pkt_loss):
             elif pacotes in pkt_loss:
                 print("B --x A : Ack", recebido)
                 pacotes += 1
+
+        if not receiver.ackNeeded() and not sender.pode_enviar():
+            sender.timeout()
--- SR2.py
+++ SR2.py
@@ -20,6 +20,7 @@ class SenderSR:
         self.numeros = []
         num = (2 ** seq_bits) - 1
         self.timers = []
+        self.naks = []
 
         for x in range(0, num_frames):
             self.timers.append(Timer())
@@ -29,53 +30,12 @@ class SenderSR:
 
         self.janela = self.janela * 100
 
-    def incrementa_timer(self):
-        for x in self.timers:
-            if x.ativo:
-                x.tempo += 1
-
-    def check_timeout(self):
-        for x in range(0, len(self.timers)):
-            if self.timers[x].ativo and self.timers[x].tempo >= 10:
-                return x
-        return None
-
-    def comeca_timer(self, frame):
-        self.timers[frame].ativo = True
-        self.timers[frame].tempo = 0
-
-    def timeout(self, reenviar):
-        self.resend(self.janela[reenviar])
-        self.timers[reenviar].ativo = True
-        self.timers[reenviar].tempo = 0
-
     def pode_enviar(self):
         if self.Sn - self.Sf < self.tam_janela and self.Sn < self.frames_a_enviar:
             return True
-
+        if len(self.naks) > 0:
+            return True
         return False
-
-    def send(self):
-        self.timers[self.Sn].ativo = True
-        self.pendentes += 1
-        self.Sn += 1
-        return self.janela[self.Sn - 1]
-
-    def receive_confirmation(self, ack):
-        if ack[0] == 'NAK':
-            if self.Sf <= self.get_pos(ack[1]) < self.Sn:
-                return self.resend(ack[1])
-        else:
-            if self.Sf < self.get_pos(ack[1]):
-                while self.Sf < self.get_pos(ack[1]):
-                    self.timers[self.Sf].ativo = False
-                    self.Sf += 1
-            return self.Sf
-
-    def resend(self, ack):
-        enviar = self.get_pos(ack)
-        self.comeca_timer(enviar)
-        return self.janela[enviar]
 
     def get_pos(self, seqno):
         x = self.Sf
@@ -87,6 +47,58 @@ class SenderSR:
 
         return 0
 
+    def comeca_timer(self, frame):
+        self.timers[frame].ativo = True
+        self.timers[frame].tempo = 0
+
+    def incrementa_timer(self):
+        for x in self.timers:
+            if x.ativo:
+                x.tempo += 1
+
+    def check_timeout(self):
+        for x in range(0, len(self.timers)):
+            if self.timers[x].ativo and self.timers[x].tempo >= 100:
+                return x
+        return None
+
+    def timeout(self, reenviar):
+        self.resend(self.janela[reenviar])
+        self.timers[reenviar].ativo = True
+        self.timers[reenviar].tempo = 0
+
+    def send(self):
+        if len(self.naks) > 0:
+            z = self.resend_nak(self.naks.pop(0)[1])
+            return 'ret', z
+
+        else:
+            self.timers[self.Sn].ativo = True
+            self.pendentes += 1
+            self.Sn += 1
+            return self.janela[self.Sn - 1]
+
+    def receive_confirmation(self, ack):
+        if ack[0] == 'NAK':
+            if self.Sf <= self.get_pos(ack[1]) < self.Sn:
+                self.naks.append(ack)
+        else:
+            if self.Sf < self.get_pos(ack[1]):
+                while self.Sf < self.get_pos(ack[1]):
+                    self.timers[self.Sf].ativo = False
+                    self.Sf += 1
+
+            return self.Sf
+
+    def resend(self, ack):
+        enviar = self.get_pos(ack)
+        self.comeca_timer(enviar)
+        return self.janela[enviar]
+
+    def resend_nak(self,nak):
+        enviar = self.get_pos(nak)
+        self.comeca_timer(enviar)
+        return self.janela[enviar]
 
 class ReceiverSR:
     def __init__(self, seq_bits, num_frames):
@@ -95,9 +107,13 @@ class ReceiverSR:
         self.janela = []
         self.tam_janela = 2 ** (seq_bits - 1)
         self.enviados = []
-        self.enviados = [False] * num_frames
+        self.enviados = [False] * (num_frames+1)
         self.ackNeeded = False
-        self.nakSent = False
+        self.nakNeeded = False
+        self.ack_fila = 0
+        self.naks = []
+        self.atrasado = False
+
         num = (2 ** seq_bits) - 1
 
         for x in range(0, num + 1):
@@ -106,21 +122,78 @@ class ReceiverSR:
         self.janela = self.janela * 100
 
     def receive(self, frame):
-        if self.janela[self.Rn] != frame and not self.nakSent:
-            self.nakSent = True
+        if self.janela[self.Rn] == frame:
             self.marca(frame)
-            return 'NAK', self.janela[self.Rn]
-        elif self.janela[self.Rn] != frame:
-            self.marca(frame)
-        else:
-            self.marca(frame)
-            while self.Rn < self.num_frames and self.enviados[self.Rn]:
-                self.Rn += 1
+            self.Rn += 1
+            self.remove_nak(frame)
             self.ackNeeded = True
+            if self.ta_atrasado(frame):
+                self.atrasado = True
+        elif self.check_duplicate(frame):
+            self.ackNeeded = True
+            self.atrasado = True
+        elif self.janela[self.Rn] != frame:
+            #print(frame,'dif')
+            self.nakNeeded = True
+            self.marca(frame)
+            self.add_naks(frame)
+            #return 'NAK', self.janela[self.Rn]
+
+    def ta_atrasado(self, frame):
+        posi = self.get_pos(frame)
+        if self.enviados[posi+1]:
+            return True
+        return False
+
+
+    def add_naks(self, frame):
+        for x in range (self.ack_fila, self.get_pos(frame)):
+            if not self.enviados[x]:
+                if ('NAK', self.janela[x]) not in self.naks:
+                    self.naks.append(('NAK', self.janela[x]))
+
+    def remove_nak(self,frame):
+        for x in range(0, len(self.naks)):
+            if self.naks[x] == frame:
+                self.naks.pop(x)
+                return
+
+    def check_duplicate(self, frame):
+        aux = self.ack_fila
+
+        while aux >= 0 and aux >= self.ack_fila - self.tam_janela:
+            if self.janela[aux] == frame and self.enviados[aux]:
+                return True
+            aux -= 1
+
+        return False
 
     def send_ack(self):
-        self.ackNeeded = self.nakSent = False
-        return 'ACK', self.janela[self.Rn]
+        aux = self.ack_fila
+        if self.atrasado:
+            self.atrasado = False
+            self.ackNeeded = False
+            while aux < self.ack_fila + self.tam_janela:
+                if self.enviados[aux]:
+                    self.ack_fila += 1
+                else:
+                    break
+                aux += 1
+            self.Rn = self.ack_fila
+            return 'ACK', self.janela[self.ack_fila]
+        else:
+            aux = self.ack_fila
+            if aux == self.Rn-1:
+                self.ackNeeded = False
+            self.ack_fila += 1
+            return 'ACK', self.janela[aux+1]
+
+    def send_nak(self):
+        self.nakNeeded = False
+        aux = self.naks
+        self.naks = []
+
+        return aux
 
     def marca(self, frame):
         x = self.Rn
@@ -132,6 +205,16 @@ class ReceiverSR:
                 break
             x += 1
 
+    def get_pos(self, frame):
+        if type(frame) is int:
+            for x in range(self.ack_fila, self.ack_fila + self.tam_janela+1):
+                if self.janela[x] == frame:
+                    return x
+        else:
+            for x in range(self.ack_fila, self.ack_fila + self.tam_janela+1):
+                if self.janela[x] == frame[1]:
+                    return x
+
 
 def executarsr(num_frames, seq_bits, pkt_loss):
     sender = SenderSR(num_frames, seq_bits)
@@ -140,48 +223,54 @@ def executarsr(num_frames, seq_bits, pkt_loss):
     pacote = 0
 
     while sender.Sf <= num_frames - 1:
-        sender.incrementa_timer()
-        check = sender.check_timeout()
-        if check is not None:
-            print('Note over A: TIMEOUT(', sender.get_pos(check)+1, ')')
-            pacote += 1
-            enviado = sender.resend(check)
-            if pacote not in pkt_loss:
-                recebido = receiver.receive(enviado)
-                print('A ->> B : (', sender.get_pos(check)+1, ') Frame ', enviado, '(RET)')
-                if recebido is not None:
-                    if recebido[0] == 'NAK':
-                        naks.append(recebido)
-            else:
-                print('A -x B : (', sender.get_pos(enviado)+1, ') Frame ', enviado, '(RET)')
-
-        elif sender.pode_enviar():
+        while sender.pode_enviar():
             pacote += 1
             enviado = sender.send()
             if pacote not in pkt_loss:
-                recebido = receiver.receive(enviado)
-                if recebido is not None:
-                    if recebido[0] == 'NAK':
-                        naks.append(recebido)
-                    else:
-                        print('A ->> B : (', sender.Sn, ') Frame ', enviado)
+                if type(enviado) is not int:
+                    receiver.receive(enviado[1])
+                    print('A ->> B : (', receiver.get_pos(enviado[1]+1), ') Frame ', enviado[1], "(RET)",pacote, sep=" ")
+                else:
+                    receiver.receive(enviado)
+                    print('A ->> B : (', sender.Sn, ') Frame ', enviado, pacote, sep=" ")
             else:
-                print('A -x B : (', sender.Sn, ') Frame ', enviado)
+                if type(enviado) is not int:
+                    print('A -x B : (', receiver.get_pos(enviado[1]), ') Frame ', enviado[1], "(RET)",pacote, sep=" ")
+                else:
+                    print('A -x B : (', sender.Sn, ') Frame ', enviado, pacote,sep=" ")
 
-        elif len(naks) > 0:
-            pacote += 1
-            if pacote not in pkt_loss:
-                recebido = sender.receive_confirmation(naks.pop(0))
-                print('B -->> A: (', sender.get_pos(recebido), 'NAK', recebido)
-            else:
-                z = naks.pop(0)
-                print('B --x A: (', sender.get_pos(z), 'NAK', z)
 
-        elif receiver.ackNeeded:
+        while receiver.ackNeeded:
             pacote += 1
             ack = receiver.send_ack()
             if pacote not in pkt_loss:
                 sender.receive_confirmation(ack)
-                print('B -->> A: (', sender.get_pos(ack[1]), 'ACK', ack[1])
+                print("B -->> A : ACK", ack[1], pacote)
             else:
-                print('B --x A: (', sender.get_pos(ack[1]), 'ACK', ack[1])
+                print("B --x A : ACK", ack[1], pacote)
+
+        if receiver.nakNeeded:
+            nak = receiver.send_nak()
+            while len(nak) > 0:
+                pacote += 1
+                aux = nak.pop(0)
+                if pacote not in pkt_loss:
+                    receiver.Rn = aux[1]
+                    print("B -->> A : NAK", aux[1], pacote)
+                    recebido = sender.receive_confirmation(aux)
+                else:
+                    print("B --x A : NAK", aux[1],pacote)
+
+        sender.incrementa_timer()
+        if not sender.pode_enviar() and not receiver.ackNeeded and not receiver.nakNeeded:
+            timeout = sender.check_timeout()
+            if timeout is not None:
+                pacote += 1
+                print('Note over A: TIMEOUT(', timeout + 1, ')', sep=" ")
+                enviado = sender.resend(sender.janela[timeout])
+                if pacote not in pkt_loss:
+                    print("A ->> B : (", timeout + 1, ") Frame ", enviado, " (RET)",pacote, sep=" ")
+                    receiver.receive(enviado)
+                else:
+                    print("A -x B : (", timeout + 1, ") Frame ", enviado, " (RET)", pacote, sep=" ")
+
