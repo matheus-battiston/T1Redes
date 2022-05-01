1bf7b206b35732857bf0b738332900b61b4a244c
matheusfelipebattiston <matheus_battiston@hotmail.com>
2022-04-30 23:59:59 -0300

    Comments

--- GBN.py
+++ GBN.py
@@ -1,36 +1,38 @@
+# Classe para simular o sender de Go Back N
 class Sender:
     def __init__(self, seq_bits):
 
-        self.primeiro_janela = 0
-        self.ultimo_janela = 0
+        self.primeiro_janela = 0  # Referencia para o primeiro frame da janela
+        self.ultimo_janela = 0  # Referencia para o ultimo frame da janela
         self.seq_bits = seq_bits
-        self.seq = []
-        self.janela = (2 ** seq_bits) - 1
-        self.timer = 0
+        self.seq = []  # Lista que irá representar a lista de sequencia
+        self.janela = (2 ** seq_bits) - 1  # Tamanho da janela
         self.prox_envio = self.primeiro_janela
-        self.pendentes = 0
+        self.pendentes = 0  # Quantidade de frames esperando confirmação
         for x in range(0, self.janela + 1):
             self.seq.append(x)
         self.seq = self.seq*100
-        self.Sn = 0
+        self.Sn = 0  # Variável para referenciar qual o proximo frame a ser enviado
 
-    def incrementa_timer(self):
-        self.timer += 1
-
+    # Função para simular um timeout
+    # Irá retornar todos os valores para o começo da janela para começar a reenviar todos os frames
     def timeout(self):
-        print('Note over A: TIMEOUT(', self.primeiro_janela+1, ')',sep="")
+        print('Note over A: TIMEOUT(', self.primeiro_janela+1, ')', sep="")
         self.prox_envio = self.primeiro_janela
-        self.timer = 0
         self.pendentes = 0
         self.ultimo_janela = self.primeiro_janela
 
-
+    # Função para definir se o sender pode enviar algum frame.
+    # Se quantidade de frames pendentes forem menor que o tamanho da janela pode enviar
     def pode_enviar(self):
         if self.pendentes < self.janela:
             return True
 
         return False
 
+    # Função para simular o envio do frame
+    # Atualiza todos os valores para o proximo envio
+    # Retorna o valor de sequencia enviado e qual deve ser o proximo enviado
     def send(self):
         self.ultimo_janela += 1
         self.prox_envio += 1
@@ -38,12 +40,12 @@ class Sender:
         self.Sn += 1
         return self.seq[self.prox_envio - 1], self.prox_envio
 
+    # Função que recebe o ack
+    # Lógica para verificar se algum frame anterior deve ser considerado confirmado também
+    # retorna quantos frames confirmados
     def receive_confirmation(self, ack):
         aux = self.primeiro_janela
         contador = 0
-        x = self.primeiro_janela
-        ultimo = self.ultimo_janela
-        self.timer = 0
 
         while ack != self.seq[aux]:
             self.primeiro_janela += 1
@@ -54,63 +56,81 @@ class Sender:
         return contador
 
 
+# Classe para simular o receiver do Go Back n
 class Receiver:
 
     def __init__(self, seq_bits):
-        self.recebidos = []
+        self.recebidos = []  # Lista que armazena os frames que forem recebidos
         self.seq_bits = seq_bits
-        self.seq = []
-        self.janela = 0
-        self.Rn = 0
+        self.seq = []  # Lista que terá os números de sequencia
+        self.Rn = 0  # Variavel para controlar qual o frame esperado.
 
         aux = (2 ** seq_bits) - 1
-        self.window = aux
         for x in range(0, aux+1):
             self.seq.append(x)
 
         self.seq = self.seq*100
 
+    # Função para simular o recebimento de um ack
+    # Irá primeiramente checar se o ack é duplicado e adicionalo novamente a lista de recebidos
+    # Caso nao seja duplicado e for o frame esperado irá considerar recebido e atualizar a variável Rn
     def receive(self, frame):
         if self.check_duplicate(frame):
             self.recebidos.append(frame)
         elif frame == self.seq[self.Rn]:
             self.recebidos.append(frame)
-            self.janela += 1
             self.Rn += 1
 
+    # Função que irá enviar um ack
+    # Pop sempre será feito no primeiro elemento da lista para confirmar em ordem
     def confirma(self):
         return self.seq[self.recebidos.pop(0) + 1]
 
+    # Função para definir se existe um ack a ser mandado
     def ackNeeded(self):
         if len(self.recebidos) > 0:
             return True
 
         return False
 
+# Função que checa se o frame recebido é duplicado
+# Garante que nao checara o numero errado caso tenham sido enviados menos frames que a o tamanho da janela
     def check_duplicate(self, frame):
-        aux = self.Rn -1
-        while aux >= 0 and aux > self.Rn - self.seq_bits:
-            if frame == self.seq[aux]:
-                return True
-            aux -= 1
+        aux = self.Rn - 1
+        if self.Rn < (2 ** self.seq_bits) - 1:
+            while aux >= 0 and aux > self.Rn:
+                if frame == self.seq[aux]:
+                    return True
+                aux -= 1
+        else:
+            while aux >= 0 and aux > self.Rn - (2 ** self.seq_bits) - 1:
+                if frame == self.seq[aux]:
+                    return True
+                aux -= 1
         return False
+
 
 def executar_gbn(num_frames, seq_bits, pkt_loss):
     sucesso = 0
     sender = Sender(seq_bits)
     receiver = Receiver(seq_bits)
     pacotes = 1
-    enviados = []
+    enviados = []  # Lista para guardar os frames enviados
 
+    # Preferencia para enviar frames. Irá continuar no laço enquanto nao forem todos confirmados
     while sucesso < num_frames:
+        # Preferencia para o envio de frames, irá continuar nesse laço enquanto puder enviar frames
         while sender.pode_enviar() and sender.ultimo_janela < num_frames:
+            # Irá simular o envio do frame
             enviado, frame = sender.send()
             if pacotes not in pkt_loss:
+                # Caso o pacote nao seja um dos que devem ser perdidos o receiver irá receber
                 receiver.receive(enviado)
+                # Este if formata o print de forma correta checando se o frame ja foi enviado
                 if frame not in enviados:
                     print("A ->> B : (", frame, ") Frame ", enviado, sep="")
                 else:
-                    print("A ->> B : (", frame, ") Frame ", enviado, " (RET)",sep="")
+                    print("A ->> B : (", frame, ") Frame ", enviado, " (RET)", sep="")
 
             else:
                 print("A -x B : (", frame, ") Frame ", enviado, sep="")
@@ -118,9 +138,12 @@ def executar_gbn(num_frames, seq_bits, pkt_loss):
             pacotes += 1
             enviados.append(frame)
 
+        # Caso nao possa enviar um frame irá tentar enviar um ack. Continuara aqui até nao ter mais acks para enviar
         while receiver.ackNeeded():
+            # Simula o envio do ack
             recebido = receiver.confirma()
-            if pacotes not in pkt_loss and (recebido != -1):
+            if pacotes not in pkt_loss:
+                # Caso o pacote nao esteja entre os que devem ser perdidos o sender receberá o ack
                 print("B -->> A : Ack", recebido)
                 confirmados = sender.receive_confirmation(recebido)
                 pacotes += 1
@@ -129,5 +152,6 @@ def executar_gbn(num_frames, seq_bits, pkt_loss):
                 print("B --x A : Ack", recebido)
                 pacotes += 1
 
+        # Caso nao possa enviar nem ack nem frame será simulado um timeout
         if not receiver.ackNeeded() and not sender.pode_enviar():
             sender.timeout()
--- SAW2.py
+++ SAW2.py
@@ -1,38 +1,45 @@
+# Classe que irá simular o sender
 class Sender:
     def __init__(self):
-        self.Sn = 0
-        self.pode_enviar = True
-        self.seq = [0, 1]*100
-        self.confirmados = 0
-        self.timer = 0
+        self.Sn = 0  # Referencia de posição na lista dos números de sequencia
+        self.pode_enviar = True  # Informação se pode ou nao ser enviado um frame
+        self.seq = [0, 1] * 100  # Lista dos números de sequencia
+        self.confirmados = 0  # Frames confirmados
 
+    # Função send - Coloca que não pode fazer um envio e incrementa o Sn, retorna o número de sequencia para o print
     def send(self):
         self.pode_enviar = False
         self.Sn += 1
         return self.seq[self.Sn - 1]
 
+    # Função para receber um ack
     def receive_confirmation(self, ack):
         if ack == self.seq[self.Sn]:
             self.pode_enviar = True
             self.confirmados += 1
 
+    # Função de reenvio de um frame. Retorna o último frame enviado utilizando a variável Sn
     def resend(self):
         self.pode_enviar = False
         return self.seq[self.Sn - 1]
 
 
+# Classe para simular o receiver
 class Receiver:
     def __init__(self):
-        self.Rn = 0
-        self.seq = [0, 1] * 100
-        self.confirma = False
+        self.Rn = 0  # Referencia de posição na lista dos numeros de sequencia
+        self.seq = [0, 1] * 100  # Lista dos numeros de sequencia
+        self.confirma = False  # Variavel para definir se pode enviar um ack
 
+# Função que simula o recebimento de um frame. Incrementa o Rn,
+# informa que pode enviar um ack e retorna o numero de sequencia do proximo ack
     def receive(self, frame):
         if frame == self.seq[self.Rn]:
             self.Rn += 1
         self.confirma = True
         return self.seq[self.Rn]
 
+# Função de envio de ack. Utiliza o Rn para retornar o numero de sequencia
     def envia_ack(self):
         self.confirma = False
         return self.seq[self.Rn]
@@ -43,29 +50,37 @@ def executar_saw(num_frames, pkt_loss):
     receiver = Receiver()
     pacotes = 0
 
+    # Simulador dos envios, Irá continuar enquanto nao tiverem sido recebidos e confirmados dos os frames
     while sender.confirmados < num_frames:
+        # Prioridade para enviar pacotes, enviará o pacote caso possa.
         if sender.pode_enviar:
             pacotes += 1
             enviado = sender.send()
             if pacotes not in pkt_loss:
+                # Caso o pacote nao seja um dos que devem ser perdidos o recebedor irá receber
+                # Caso contrario nada irá acontecer.
                 receiver.receive(enviado)
-                print('A ->> B : (', sender.Sn, ') Frame ', enviado,sep="")
+                print('A ->> B : (', sender.Sn, ') Frame ', enviado, sep="")
             else:
-                print('A -x B : (', sender.Sn, ') Frame ', enviado,sep="")
-
+                print('A -x B : (', sender.Sn, ') Frame ', enviado, sep="")
+        # Caso o sender nao possa enviar o pacote e o recebedor possa confirmar o pacote será enviado o ack
         elif receiver.confirma:
             pacotes += 1
             confirma = receiver.envia_ack()
             if pacotes not in pkt_loss:
+                # Caso o pacote nao seja um dos que devem ser perdidos o sender receberá o ack
+                # Caso contrario nada acontece
                 sender.receive_confirmation(confirma)
                 print("B -->> A : Ack", confirma)
             else:
                 print("B --x A : Ack", confirma)
         else:
-            print('Note over A: TIMEOUT(', sender.Sn, ')',sep="")
+            # Se nao possa enviar frame ou receber ack sera simulado um timeout e o frame será reenviado
+            print('Note over A: TIMEOUT(', sender.Sn, ')', sep="")
             pacotes += 1
             enviado = sender.resend()
             if pacotes not in pkt_loss:
+                # Caso o pacote nao seja um dos que devem ser perdidos o receiver receberá o pacote
                 print("A ->> B : (", sender.Sn, ") Frame ", enviado, ' (RET)', sep="")
                 receiver.receive(enviado)
             else:
